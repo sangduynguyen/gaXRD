1c37b66659328d7af3caa43df88d47f0810bedc2
sangduynguyen <61673596+sangduynguyen@users.noreply.github.com>
2022-12-27 23:45:07 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# main.py
 import sys
 try:
- x = int(input('Choose task (Baseline:1, XRD:2): '))
+ x = int(input('Choose task (Baseline:1, XRD Gaussiaan:2, XRD Lorentzian:3): '))
  if x == 1:
     print('You have chosen baseline.py \nPlease go to the csv file and select the XRD spectrum to analyze!')
     import baseline
  elif x == 2:
     print('You have chosen XRD.py \nPlease go to the csv file and select the XRD spectrum to analyze!')
     import XRD
+ elif x == 3:
+    print('You have chosen XRD_LR.py \nPlease go to the csv file and select the XRD spectrum to analyze!')
+    import XRD_LR
  else:
     print('You have not selected anything, please go to main function to run it again')
 except:
